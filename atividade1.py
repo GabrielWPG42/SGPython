4620df7bfa255db684d71a3dced3697c40513c67
GabrielWPG42 <gabriel.wpg.42@gmail.com>
2025-06-12 19:08:44 -0300

    Create atividade1.py
    primeira versão do código da atividade 1 do curso SG Python

---
+++ atividade1.py
@@ -0,0 +1,15 @@
+printPython2 = "eu sei fazer print no python 2 assim e assim: "
+printPython3 = "eu sei fazer print no python 3 assim tambem e assim: "
+
+print printPython2 + "hello World!"
+print (printPython3 + "hello World!")
+
+quantidadeCaracteresPrintPython2 = "47"
+quantidadeCaracteresPrintPython3 = "54"
+
+print "printPython2 tem " + quantidadeCaracteresPrintPython2 + "caracteres e printPython3 tem " + quantidadeCaracteresPrintPython3 + "caracteres"
+
+modPython2ePython3 = 54 % 47
+
+print("Eis o resto da divisao de quantidadeCaracteresPrintPython2 e quantidadeCaracteresPrintPython3: ")
+print modPython2ePython3
