f9b3710ad5a7c7e8384743665baa9990eaafb2ac
GabrielWPG42 <gabriel.wpg.42@gmail.com>
2025-09-19 16:03:57 -0300

    Create caioIfElse.py
    caioIfElse.py

---
+++ caioIfElse.py
@@ -0,0 +1,10 @@
+quantidadeDeVida=50
+quantidadeDeFlechas=53
+acertou=False
+if acertou:
+    if quantidadeDeFlechas>quantidadeDeVida:
+        print ("matou o boss")
+    else:
+        print("faltou flecha")
+else:
+    print("voce perdeu")
